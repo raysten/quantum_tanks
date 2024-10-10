d5408371dd272d4097e20f41cf98fcfd3c46b4db
raysten <raysten87@gmail.com>
2024-10-10 09:27:41 +0200

    Assign initial spawn positions depending on player index.

--- Assets/QuantumUser/Simulation/Tank/TankSpawnSystem.cs
+++ Assets/QuantumUser/Simulation/Tank/TankSpawnSystem.cs
@@ -1,3 +1,4 @@
+using Photon.Deterministic;
 using UnityEngine.Scripting;
 
 namespace Quantum
@@ -5,6 +6,8 @@ namespace Quantum
     [Preserve]
     public unsafe class TankSpawnSystem : SystemSignalsOnly, ISignalOnPlayerAdded
     {
+        private static readonly FP _xPosition = 3;
+
         public void OnPlayerAdded(Frame frame, PlayerRef player, bool firstTime)
         {
             var playerData = frame.GetPlayerData(player);
@@ -22,6 +25,39 @@ namespace Quantum
 
                 tank->TankRotator = rotatorEntity;
             }
+            
+            AssignInitialPosition(frame, player, tankEntity, tank->TankRotator);
+        }
+
+        private void AssignInitialPosition(Frame frame, PlayerRef player, EntityRef tankEntity, EntityRef rotatorEntity)
+        {
+            var xPosition = CalculateXPosition(player);
+            
+            if (frame.Unsafe.TryGetPointer<Transform3D>(tankEntity, out var tankTransform))
+            {
+                tankTransform->Position = new FPVector3(xPosition, 0, 0);
+            }
+            
+            if (frame.Unsafe.TryGetPointer<Transform3D>(rotatorEntity, out var rotatorTransform))
+            {
+                rotatorTransform->Position = new FPVector3(xPosition, -FP._0_10, 0);
+            }
+        }
+
+        private FP CalculateXPosition(PlayerRef player)
+        {
+            FP xPosition;
+
+            if (player == 0)
+            {
+                xPosition = -_xPosition;
+            }
+            else
+            {
+                xPosition = _xPosition;
+            }
+
+            return xPosition;
         }
     }
 }
